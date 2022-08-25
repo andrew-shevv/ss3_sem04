3e0d482e35b1acfbb5cb4c8895001ba8c261c9e4
Andrew <vishnyaishere@gmail.com>
2022-08-25 18:39:53 +0300

    exc001 done

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -5,3 +5,11 @@
 // 4 -> 10
 // 8 -> 36
 
+Console.WriteLine("Input number: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for(int i = 0; i <= numA; i++){
+    count += i;
+}
+
+Console.WriteLine($"{count} - sum of numbers from 1 to {numA}");
