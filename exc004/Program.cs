3923201d2faea25193850d2081e073d52d0222e6
Andrew <vishnyaishere@gmail.com>
2022-08-25 19:19:49 +0300

    exc004 done

--- exc004/Program.cs
+++ exc004/Program.cs
@@ -3,3 +3,12 @@
 // заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++){
+    array[i] = new Random().Next(0, 2);
+}
+
+for (int i = 0; i < array.Length; i++){
+Console.Write($"{array[i]} ");
+}
