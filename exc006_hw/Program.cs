857333a83bbaf3369382c521c34fb3785f2cab6d
Andrew <vishnyaishere@gmail.com>
2022-08-25 20:27:33 +0300

    exc006 done

--- exc006_hw/Program.cs
+++ exc006_hw/Program.cs
@@ -5,3 +5,14 @@
 // 82 -> 10
 // 9012 -> 12
 
+Console.WriteLine("Input number: ");
+int num = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+
+int sum = 0;
+while (num > 0){
+    int numLast = num % 10;
+    sum += numLast;
+    num /= 10;
+}
+
+Console.WriteLine(sum);
