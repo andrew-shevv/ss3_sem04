fba0ded729adac96952badbcd4e24c7e68ba55ae
Andrew <vishnyaishere@gmail.com>
2022-08-25 19:06:48 +0300

    exc002 done

--- exc002/Program.cs
+++ exc002/Program.cs
@@ -6,3 +6,14 @@
 // 89126 -> 5
 // Не используем в решении задачи строки и ее свойства
 
+Console.WriteLine("Input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+num = Math.Abs(num);
+
+int count = 1;
+while (num >= 10){
+    num /= 10;
+    count++;
+}
+
+Console.WriteLine($"{count} - amount of digits in this number");
