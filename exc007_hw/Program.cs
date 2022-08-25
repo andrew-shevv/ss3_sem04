df601d829acc452e60fe9dbcbec89ce35f04d088
Andrew <vishnyaishere@gmail.com>
2022-08-25 20:50:53 +0300

    exc007 done

--- exc007_hw/Program.cs
+++ exc007_hw/Program.cs
@@ -3,3 +3,15 @@
 // и выводит их на экран. 
 // Вводим массив через консоль
 
+int[] array = new int[8];
+
+for (int i = 0; i < array.Length; i++){
+    Console.Write($"Input element of array ({i + 1} out of {array.Length}): ");
+    array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine();
+Console.WriteLine("Your array: ");
+for (int i = 0; i < array.Length; i++){
+    Console.Write($"{array[i]} ");
+}
