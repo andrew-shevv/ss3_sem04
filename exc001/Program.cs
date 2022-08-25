7974c46fe40122e17618346f746b1ab6bba2cb6a
Andrew <vishnyaishere@gmail.com>
2022-08-25 18:59:43 +0300

    exc001 corrected

--- exc001/Program.cs
+++ exc001/Program.cs
@@ -7,9 +7,9 @@
 
 Console.WriteLine("Input number: ");
 int numA = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-for(int i = 0; i <= numA; i++){
-    count += i;
+int sum = 0;
+for(int i = 1; i <= numA; i++){
+    sum += i;
 }
 
-Console.WriteLine($"{count} - sum of numbers from 1 to {numA}");
+Console.WriteLine($"{sum} - sum of numbers from 1 to {numA}");
